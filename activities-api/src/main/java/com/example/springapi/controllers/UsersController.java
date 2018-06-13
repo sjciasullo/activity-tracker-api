9660208ec8da50f96e996b424d9a5c8dca771393
Steven Ciasullo <sjciasullo@gmail.com>
2018-06-13 08:03:30 -0400

    move cors config to gateway

--- activities-api/src/main/java/com/example/springapi/controllers/UsersController.java
+++ activities-api/src/main/java/com/example/springapi/controllers/UsersController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class UsersController {
 
     @Autowired
